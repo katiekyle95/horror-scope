1910d016f5185aeda7c4e9055232d4a59d0c8114
Katie <47231728+hootpoot@users.noreply.github.com>
2019-05-28 23:37:40 -0500

    finished initial routing

--- horror-scope/src/index.js
+++ horror-scope/src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import App from './App';
-import Landing from './Pages/landing';
+import Movie from './Pages/movie';
 import Profile from './Pages/profile';
+import Results from './Pages/profile';
+
 const routing = (
   <Router>
     <div>
       <Switch>
         <Route exact path="/" component={App} />
-        <Route path="/landing" component={Landing} />
+        <Route path="/movie/:movieId" component={Movie} />
         <Route path="/profile" component={Profile} />
-        
+        <Route path="search/:name" component={Results} />
       </Switch>
     </div>
   </Router>
